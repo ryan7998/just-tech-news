afd51c5f3c618aa4eef8107a637ec3364f453fff
fazle <ryan7998@gmail.com>
2021-03-06 15:04:38 -0500

    session added

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -63,10 +63,18 @@ router.post('/', (req, res) => {
       email: req.body.email,
       password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+        
+        res.json(dbUserData);
+      });
+    })
     .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
@@ -86,7 +94,15 @@ router.post('/login', (req, res) => {
         res.status(400).json({ message: 'Incorrect password!' });
         return;
       }
-      res.json({ use:dbUserData, message: 'You are now logged in!' });
+
+      req.session.save(() => {
+        // declare session variables
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+      })
     });
 });
 
@@ -134,4 +150,14 @@ router.delete('/:id', (req, res) => {
     });
 });
 
+//Log out:
+router.post('/logout', (req, res) => {
+  if(req.session.loggedIn){
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+})
 module.exports = router;
